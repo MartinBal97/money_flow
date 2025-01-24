917f213738b4cb2341462ddbcb53367a558a5ec0
Martin Balverde <tinchoabalverde@gmail.com>
2025-01-24 20:27:41 +0100

    advances adding incomes and expenses

--- lib/presentation/utils/utils.dart
+++ lib/presentation/utils/utils.dart
@@ -30,3 +30,55 @@ String? nameValidator(String? value) {
   }
   return null;
 }
+
+String? dateValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Por favor, llena este campo.';
+  }
+  final dateRegex = RegExp(r'^\d{2}/\d{2}/\d{4}$');
+  if (!dateRegex.hasMatch(value)) {
+    return 'La fecha debe tener el formato dd/mm/yyyy.';
+  }
+
+  try {
+    final parts = value.split('/');
+    final day = int.parse(parts[0]);
+    final month = int.parse(parts[1]);
+    final year = int.parse(parts[2]);
+
+    final date = DateTime(year, month, day);
+
+    // Verifica si la fecha es válida
+    if (date.year != year || date.month != month || date.day != day) {
+      return 'La fecha no es válida.';
+    }
+  } catch (_) {
+    return 'La fecha no es válida.';
+  }
+
+  return null;
+}
+
+String? moneyValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Por favor, llena este campo.';
+  }
+  // Expresión regular para validar números enteros o decimales (con punto o coma como separador)
+  final moneyRegex = RegExp(r'^\d+([.,]\d{1,2})?$');
+  if (!moneyRegex.hasMatch(value)) {
+    return 'Ingresa una cantidad válida. Ejemplo: 1234, 1234.56, 1234,56';
+  }
+
+  // Reemplaza la coma por un punto para trabajar con decimales si es necesario
+  final sanitizedValue = value.replaceAll(',', '.');
+  try {
+    final amount = double.parse(sanitizedValue);
+    if (amount < 0) {
+      return 'La cantidad no puede ser negativa.';
+    }
+  } catch (_) {
+    return 'Ingresa una cantidad válida.';
+  }
+
+  return null;
+}
